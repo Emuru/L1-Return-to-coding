0e0a1c3e37106896cc5cc46eb97ce17a26eda32f
Emil Meri <em223ve@student.lnu.se>
2024-09-08 21:32:43 +0200

    Add image fetcher

--- src/js/index.js
+++ src/js/index.js
@@ -13,11 +13,40 @@ inputForm.addEventListener('submit', (event) => {
   showGreeting()
 })
 
-function showGreeting() {
+async function showGreeting() {
   const name = document.querySelector('#name').value
 
   greeting.textContent = `Hello ${name}!`
 
+  const imageUrl = await fetchImage(name)
+
+  document.body.style.backgroundImage = `url(${imageUrl})`
+
   inputForm.classList.toggle('hidden')
   greeting.classList.toggle('hidden')
+}
+
+async function fetchImage(name) {
+  try {
+    const url = `https://api.unsplash.com/photos/random/?orientation=landscape&auto=format&query=${name}`
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Client-ID `
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Image not found')
+    }
+
+    const data = await response.json()
+
+    const imageUrl = data.urls.full
+
+    return imageUrl
+  } catch (error) {
+    console.log(error)
+  }
 }
